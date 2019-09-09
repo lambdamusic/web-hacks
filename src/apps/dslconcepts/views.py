fa3e15451b7a3b008724bd91dddef7e06612dec9
lambdamusic <michele.pasin@gmail.com>
2019-09-09 19:05:04 +0100

    first iteration

--- src/apps/dslconcepts/views.py
+++ src/apps/dslconcepts/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 import requests
 import json
 import sys
+from collections import Counter
 #
 from libs.myutils.lists import batches
 from libs.myutils.myutils import print_json
@@ -18,7 +19,7 @@ except:
 
 DEBUG_MODE = False  # set to True to print out useful info in the console
 
-#
+
 QUERY = """
     search publications in concepts for "{}" 
     return publications[basics+concepts+doi+times_cited] sort by times_cited limit 100 
@@ -30,24 +31,50 @@ def home(request):
     """
     home page (and possibly the only one)
     """
-    # topics1, topics2 = batches(TOPICS, 2)
-    # res, tot, q = None, None, None
-    searchterm = request.GET.get("query", "")
-    # restrict = request.GET.get("restrict", "")
-    # country = request.GET.get("country", "")
+    URL_AND_SYMBOL = "AND"
+    res, tot, q, related_concepts = None, None, None, {}
+    searchquery = request.GET.get("query", "")
+    if searchquery.endswith(URL_AND_SYMBOL):
+        searchquery = searchquery[:-3]
+    search_concepts = searchquery.split(URL_AND_SYMBOL)
 
-    if searchterm:
-        q = QUERY.format(searchterm)
+    # print(searchquery)
+    # print(search_concepts)
+
+    def search_terms_join(s):
+        "from a 'AND' list make a proper concepts search query"
+        bits = s.strip().split(URL_AND_SYMBOL)
+        return " AND ".join(["\\\"{}\\\"".format(x) for x in bits if x])
+
+    def count_concepts(res):
+        c = []
+        for p in res['publications']:
+            c += p['concepts']
+        return Counter(c)
+
+
+    if searchquery:
+        q = QUERY.format(search_terms_join(searchquery))
         print(q)
         res = do_query(q)
-        print(res.keys())
-        # res = json.loads(res)
-        tot = res['_stats']['total_count']
+        
+        try:
+            tot = res['_stats']['total_count']
+            print("Results ==>", tot)
+        except:
+            print(res.keys())
+        
+        all_concepts = count_concepts(res)
+        related_concepts  = all_concepts.most_common(400)
+        # print(related_concepts)
 
     context = {
         'res': res,
         'tot': tot,
-        'query': searchterm,
+        'searchquery': searchquery,
+        'search_concepts': search_concepts,
+        # https://docs.python.org/3/library/collections.html#collections.Counter.most_common
+        'related_concepts': related_concepts,
     }
 
     return render(request, 'dslconcepts/search.html', context)
